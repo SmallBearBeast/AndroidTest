a572df7472eb40d6d020593970a53bc17fa8d36d
wuyisong <wuyisong@bigo.sg>
2019-05-11 17:28:35 +0800

    add ClipDrawable Test

--- app/src/main/java/com/example/administrator/androidtest/Test/DrawableTest/DrawableTestAct.java
+++ app/src/main/java/com/example/administrator/androidtest/Test/DrawableTest/DrawableTestAct.java
@@ -2,7 +2,9 @@ package com.example.administrator.androidtest.Test.DrawableTest;
 
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ClipDrawable;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +16,7 @@ public class DrawableTestAct extends ComponentAct {
     private static final String TAG = "DrawableTestAct";
     private View mVTest_1;
     private View mVTest_2;
+    private View mVTest_4;
     @Override
     protected int layoutId() {
         return R.layout.act_drawable_test;
@@ -25,6 +28,7 @@ public class DrawableTestAct extends ComponentAct {
         new BitmapDrawable();
         mVTest_1 = findViewById(R.id.v_test_1);
         mVTest_2 = findViewById(R.id.v_test_2);
+        mVTest_4 = findViewById(R.id.v_test_4);
         ColorDrawable drawable_1 = (ColorDrawable) getResources().getDrawable(R.drawable.color_drawable_test);
         ColorDrawable drawable_2 = (ColorDrawable) getResources().getDrawable(R.drawable.color_drawable_test);
 //        ColorDrawable drawable_1 = new ColorDrawable(Color.BLACK);
@@ -33,5 +37,7 @@ public class DrawableTestAct extends ComponentAct {
         drawable_1.setColor(Color.RED);
         mVTest_1.setBackground(drawable_1);
         mVTest_2.setBackground(drawable_2);
+        Drawable drawable = mVTest_4.getBackground();
+        drawable.setLevel(5000);
     }
 }
