29ea2fd1f61334f81c0b1f160ded3160958bb94a
wuyisong <wuyisong@bigo.sg>
2019-10-11 09:38:08 +0800

    feature: add OnceAction

--- libbase/src/main/java/com/example/libbase/OnceAction.java
+++ libbase/src/main/java/com/example/libbase/OnceAction.java
@@ -1,4 +1,19 @@
 package com.example.libbase;
 
 public class OnceAction {
+    private boolean mIsDone = false;
+    private Runnable mAction;
+
+    public OnceAction(Runnable action) {
+        mAction = action;
+    }
+
+    public void doOnceAction() {
+        if (!mIsDone) {
+            mIsDone = true;
+            if (mAction != null) {
+                mAction.run();
+            }
+        }
+    }
 }
