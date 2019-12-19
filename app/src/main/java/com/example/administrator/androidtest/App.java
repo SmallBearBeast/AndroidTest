6161213b1c3afc16e3dad9d4772761e58e7a85b0
wuyisong <wuyisong@bigo.sg>
2019-12-19 16:52:50 +0800

    add LeakCanary

--- app/src/main/java/com/example/administrator/androidtest/App.java
+++ app/src/main/java/com/example/administrator/androidtest/App.java
@@ -8,6 +8,8 @@ import android.util.Log;
 
 import com.example.libbase.Util.AppInitUtil;
 import com.example.libfresco.FrescoUtil;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +19,7 @@ public class App extends Application {
     private static final String TAG = "App";
     private static Context mContext;
     public static Map<String, Boolean> FragVisibiableMap = new HashMap<>();
+    private RefWatcher mRefWatcher;
 
     @Override
     public void onCreate() {
@@ -25,6 +28,12 @@ public class App extends Application {
         AppInitUtil.init(this);
         FrescoUtil.init(this);
         registerActivityLifecycleCallbacks(new Callback());
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        mRefWatcher = LeakCanary.install(this);
     }
 
     public static Context getContext() {
@@ -69,5 +78,10 @@ public class App extends Application {
             Log.e(TAG, "onActivityDestroyed: " + "class = " + activity.getClass().getSimpleName() + "     ");
             String s = null;
         }
+    }
+
+    public static RefWatcher getRefWatcher(Context context) {
+        App application = (App) context.getApplicationContext();
+        return application.mRefWatcher;
     }
 }
