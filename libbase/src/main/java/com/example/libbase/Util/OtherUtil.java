8c218ef6ba09fcfe0cff104e8382cf2f71e5b056
wuyisong <wuyisong@bigo.sg>
2020-03-18 16:14:02 +0800

    adjust OtherUtil

--- libbase/src/main/java/com/example/libbase/Util/OtherUtil.java
+++ libbase/src/main/java/com/example/libbase/Util/OtherUtil.java
@@ -1,5 +1,8 @@
 package com.example.libbase.Util;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -7,6 +10,9 @@ import android.graphics.Matrix;
 
 import androidx.core.text.TextUtilsCompat;
 import androidx.core.view.ViewCompat;
+
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 
 public class OtherUtil extends AppInitUtil {
@@ -29,5 +35,29 @@ public class OtherUtil extends AppInitUtil {
         Matrix matrix = new Matrix();
         matrix.postScale(-1, 1, 0.5f, 0.5f);
         iv.setImageBitmap(Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, false));
+    }
+
+    public static void copyToClipboard(final String label, final String text) {
+        ClipboardManager manager = (ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData data = ClipData.newPlainText(label, text);
+        if(manager != null) {
+            try {
+                manager.setPrimaryClip(data);
+            } catch (Exception e){
+
+            }
+        }
+    }
+
+    public static void showSoftKeyboard(final Context context, final View view) {
+        try {
+            view.requestFocus();
+            final InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+            if(imm != null) {
+                imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
+            }
+        } catch (NullPointerException e) {
+
+        }
     }
 }
