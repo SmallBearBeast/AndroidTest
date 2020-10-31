8dc20c3d08933f7faa42005b85cb960f01c26309
hugo.wu <hugo.wu@ringcentral.com>
2020-10-31 17:13:09 +0800

    fix: adjust OtherTestAct.java

--- app/src/main/java/com/example/administrator/androidtest/Test/OtherTest/OtherTestAct.java
+++ app/src/main/java/com/example/administrator/androidtest/Test/OtherTest/OtherTestAct.java
@@ -1,22 +1,43 @@
 package com.example.administrator.androidtest.Test.OtherTest;
 
+import android.os.Build;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 
 import com.bear.libcomponent.ComponentAct;
 import com.example.administrator.androidtest.R;
 
 public class OtherTestAct extends ComponentAct {
+    private EditText editText;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        editText = findViewById(R.id.et_no_show_input_keyboard);
+    }
+
     @Override
     protected int layoutId() {
-        return R.layout.act_commom_test;
+        return R.layout.act_other_test;
     }
 
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.bt_1:
-                TextView textTv = findViewById(R.id.tv_text_1);
+            case R.id.bt_format_text_click:
+                TextView textTv = findViewById(R.id.tv_format_text);
                 textTv.setText(getString(R.string.format_string_text, "hugo.wu", 22));
+                break;
+
+            case R.id.bt_no_show_input_keyboard_mask_click:
+                // EditText get focus but not to show input keyboard
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    editText.setShowSoftInputOnFocus(false);
+                    editText.setCursorVisible(false);
+                }
                 break;
         }
     }
