332a3083a09676dad3ba54dac0d2606875fbd8e6
hugo.wu <hugo.wu@ringcentral.com>
2020-11-03 12:27:47 +0800

    fix: arrange setupNoShowInput

--- app/src/main/java/com/example/administrator/androidtest/Test/OtherTest/OtherTestAct.java
+++ app/src/main/java/com/example/administrator/androidtest/Test/OtherTest/OtherTestAct.java
@@ -3,6 +3,9 @@ package com.example.administrator.androidtest.Test.OtherTest;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,6 +17,9 @@ import com.example.administrator.androidtest.R;
 import com.example.administrator.androidtest.Widget.FullTextView.FullTextView;
 import com.example.administrator.androidtest.Widget.FullTextView.TextOpt;
 import com.example.administrator.androidtest.Widget.LikeView.LikeView;
+import com.example.libbase.Util.KeyBoardUtil;
+import com.example.libbase.Util.OtherUtil;
+import com.example.libbase.Util.ToastUtil;
 
 public class OtherTestAct extends ComponentAct {
     private FullTextView ftvFullText;
@@ -33,7 +39,7 @@ public class OtherTestAct extends ComponentAct {
         return R.layout.act_other_test;
     }
 
-    public void onClick(View view) {
+    public void onClick(final View view) {
         switch (view.getId()) {
             case R.id.bt_format_text_click:
                 TextView textTv = findViewById(R.id.tv_format_text);
@@ -42,10 +48,7 @@ public class OtherTestAct extends ComponentAct {
 
             case R.id.bt_no_show_input_keyboard_mask_click:
                 // EditText get focus but not to show input keyboard
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    editText.setShowSoftInputOnFocus(false);
-                    editText.setCursorVisible(false);
-                }
+                setupNoShowInput();
                 break;
 
             case R.id.bt_full_text_click:
@@ -61,6 +64,54 @@ public class OtherTestAct extends ComponentAct {
                     likeView.unLike();
                 }
                 break;
+        }
+    }
+
+    private void setupNoShowInput() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            editText.setShowSoftInputOnFocus(false);
+            editText.setCursorVisible(false);
+            editText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ToastUtil.showToast("Click No Show Input Keyboard EditText");
+                    OtherUtil.hideSoftInput(OtherTestAct.this, v);
+                }
+            });
+            editText.setOnLongClickListener(null);
+            editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (hasFocus) {
+                        OtherUtil.hideSoftInput(OtherTestAct.this, v);
+                    }
+                }
+            });
+            editText.setCustomSelectionActionModeCallback(new ActionMode.Callback() {
+                @Override
+                public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                    return false;
+                }
+
+                @Override
+                public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                    return false;
+                }
+
+                @Override
+                public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                    return false;
+                }
+
+                @Override
+                public void onDestroyActionMode(ActionMode mode) {
+
+                }
+            });
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            editText.setImportantForAutofill(View.IMPORTANT_FOR_AUTOFILL_NO);
         }
     }
 }
