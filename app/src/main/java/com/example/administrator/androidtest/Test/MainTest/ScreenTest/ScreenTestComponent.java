ab2718673805501c2bc9e13c7d52e3da7c7be99c
Hugo.wu <hugo.wu@ringcentral.com>
2022-04-15 22:58:31 +0800

    Fix: Click screenTestButton not jump

--- app/src/main/java/com/example/administrator/androidtest/Test/MainTest/ScreenTest/ScreenTestComponent.java
+++ app/src/main/java/com/example/administrator/androidtest/Test/MainTest/ScreenTest/ScreenTestComponent.java
@@ -9,14 +9,14 @@ import com.example.administrator.androidtest.Test.MainTest.TestComponent;
 public class ScreenTestComponent extends TestComponent {
     @Override
     protected void onCreate() {
-        clickListener(this, R.id.screenLabelTextView);
+        clickListener(this, R.id.screenTestButton);
     }
 
     @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.screenLabelTextView:
+            case R.id.screenTestButton:
                 ScreenAct.start(getDependence());
                 break;
 
