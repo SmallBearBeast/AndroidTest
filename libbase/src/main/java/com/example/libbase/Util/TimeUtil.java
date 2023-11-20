d26549e677b18de8062fd0d503db0530a8100ecf
Hugo.Wu <smallbearbeast@gmail.com>
2023-11-20 23:15:26 +0800

    fix: Remove name when log start/warm up

--- libbase/src/main/java/com/example/libbase/Util/TimeUtil.java
+++ libbase/src/main/java/com/example/libbase/Util/TimeUtil.java
@@ -6,8 +6,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TimeUtil {
-    private static Map<String, Long> sStartTimeMap = new HashMap<>();
-    private static Map<String, Long> sEndTimeMap = new HashMap<>();
+    private static final Map<String, Long> sStartTimeMap = new HashMap<>();
+    private static final Map<String, Long> sEndTimeMap = new HashMap<>();
 
     public static void markStart(String name){
         synchronized (TimeUtil.class) {
